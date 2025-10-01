ce1625845c044baddcd1dfa2b224df772dbe32d1
Pierugo Pace <pierugo.pace@gmail.com>
2025-10-01 22:30:05 +0200

    docs(install.sh): rootless Docker installation (#4)

--- install.sh
+++ install.sh
@@ -62,7 +62,31 @@ use_rsyslog() {
 install_docker() {
     # Install Docker
     echo "Installing Docker..."
-    curl -fsSL https://get.docker.com | sh
+    apt-get update
+    apt-get install -y ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
+
+    apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    echo "Do you want to setup rootless Docker? (y/n)"
+    read -r rootless_docker_answer
+    if [[ "$rootless_docker_answer" == "y" ]]; then
+        apt-get install -y uidmap docker-ce-rootless-extras
+
+        systemctl disable --now docker.service docker.socket
+        rm /var/run/docker.sock
+
+        echo "Create a new unprivileged user and continue following the instructions with this user here: https://docs.docker.com/engine/security/rootless/"
+        exit 0
+    fi
 
     echo "Docker successfully installed!"
 }
