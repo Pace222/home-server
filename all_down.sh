26e0b404a42a548b4d9d6933de023d378f2a3315
Pierugo Pace <pierugo.pace@gmail.com>
2025-11-11 20:05:38 +0100

    feat(scripts): helper scripts to up/down all containers

---
+++ all_down.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+docker compose -f ddclient/docker-compose.yml down
+docker compose -f fw-monitor/docker-compose.yml down
+
+docker compose -f int-homepage/docker-compose.yml down
+docker compose -f grafana/docker-compose.yml down
+docker compose -f int-proxy/docker-compose.yml down
+
+docker compose -f dabloon-clicker/docker-compose.yml down
+docker compose -f ext-homepage/docker-compose.yml down
+docker compose -f authentik/docker-compose.yml down
+docker compose -f ext-proxy/docker-compose.yml down
+
+docker compose -f wireguard/docker-compose.yml down
+
+docker compose -f pihole/docker-compose.yml down
+
+docker compose -f telegram-bot/docker-compose.yml down
+
+docker compose -f monitoring/docker-compose.yml down
+docker compose -f prometheus/docker-compose.yml down
---
+++ all_up.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+docker compose -f prometheus/docker-compose.yml up -d
+docker compose -f monitoring/docker-compose.yml up -d
+
+docker compose -f telegram-bot/docker-compose.yml up -d
+
+docker compose -f pihole/docker-compose.yml up -d
+
+docker compose -f wireguard/docker-compose.yml up -d
+
+docker compose -f ext-proxy/docker-compose.yml up -d
+docker compose -f authentik/docker-compose.yml up -d
+docker compose -f ext-homepage/docker-compose.yml up -d
+docker compose -f dabloon-clicker/docker-compose.yml up -d
+
+docker compose -f int-proxy/docker-compose.yml up -d
+docker compose -f grafana/docker-compose.yml up -d
+docker compose -f int-homepage/docker-compose.yml up -d
+
+docker compose -f fw-monitor/docker-compose.yml up -d
+docker compose -f ddclient/docker-compose.yml up -d
