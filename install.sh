e4bb4a708c6d2140b60d577537ab1573d38f307c
Pierugo Pace <pierugo.pace@gmail.com>
2025-12-05 22:52:32 +0100

    feat(media): add Radarr (#25)

--- install.sh
+++ install.sh
@@ -153,6 +153,7 @@ create_networks() {
     docker network create --internal --subnet=172.19.5.0/24 --gateway=172.19.5.1 net-fw-monitor
     docker network create --internal --subnet=172.19.6.0/26 --gateway=172.19.6.1 net-grafana
     docker network create --internal --subnet=172.19.7.0/26 --gateway=172.19.7.1 net-loki
+    docker network create --internal --subnet=172.19.8.0/29 --gateway=172.19.8.1 net-int-media
 
     # Communication with Prometheus
     docker network create --internal --subnet=172.21.1.0/24 --gateway=172.21.1.1 prom-wg
